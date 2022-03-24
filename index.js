e5e9fc3b3f6d598ea87dff612c49bb2e23138e04
Norman Smart <normansmart14@gmail.com>
2022-03-24 19:53:12 -0400

    commit tree 74058d7c84833d8c4b31c5ed84953207aaf7cb3f

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function introduction(name){
+
+    const name = 'Aki'
+
+    console.log(`Hi, my name is ${name}.`);
+    
+}
+
+
+
+function introductionWithLanguage(name, language){
+
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+
+}
+
+
